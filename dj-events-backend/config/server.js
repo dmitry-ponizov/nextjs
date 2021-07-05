12deacdbfa72edde7623dbef10570572c390c44d
Dmitry Ponizov <dmitry.ponizov@ideasoft.io>
2021-07-05 14:27:40 +0300

    first commit

---
+++ dj-events-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b173bd2782591dba9489f44fb183268c'),
+    },
+  },
+});
---
+++ dj-events-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '471fd174-1d90-4106-8083-5c76bea96c29'
+};
